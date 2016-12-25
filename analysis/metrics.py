a8845c9d22fd19d54d6576bf4af4c36ca16e20ed
Michael Zhu <mzhu@tigertext.com>
2016-12-24 20:04:24 -0800

    some fancy arg parsing

--- analysis/metrics.py
+++ analysis/metrics.py
@@ -1,17 +1,26 @@
 import json, sys, os, argparse, math
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--month', '-m', required=True)
-parser.add_argument('--tier', '-t', required=True)
+parser.add_argument('--month', '-t', required=False)
+parser.add_argument('--format', '-f', required=True)
+parser.add_argument('--metric', '-m', required=False)
+parser.add_argument('--degree', '-d', required=False)
+parser.add_argument('--epsilon', '-e', required=False)
 args = parser.parse_args()
 
+all_months = ['2014-11', '2014-12', '2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06', \
+              '2015-07', '2015-08', '2015-09', '2015-10', '2015-11', '2015-12', '2016-01', '2016-02', \
+              '2016-03', '2016-04', '2016-05', '2016-06', '2016-07', '2016-08', '2016-09', '2016-10', '2016-11']
 data = None
 info = None
 
-def read_file():
+epsilon = args.epsilon or 0
+q = args.degree or 2
+
+def read_file(month, format):
     global data
     global info
-    file_path = 'data/' + args.month + '/' + args.tier + '.json'
+    file_path = 'data/' + month + '/' + format + '.json'
     with open(file_path, 'rb') as data_file:
         data = json.load(data_file)
         info = data['info']
@@ -29,18 +38,18 @@ def gini():
     return numerator/float(denominator)
 
 # Number of pokemon with usage percentage above some threshold epsilon
-def richness(epsilon):
+def richness():
     counter = 0
     for pokemon in data:
         if pokemon[1]['Raw count']/float(info['number of battles']) > epsilon:
             counter += 1
     return counter
 
-def diversity(q, epsilon=0):
+def diversity():
     # The limit of the diversity expression is undefined but well-defined as q -> 1, and equals 
     # the exponential of the Shannon entropy
     if q == 1:
-        return math.exp(shannon(epsilon))
+        return math.exp(shannon())
     wgm = 0 # weighted generalized mean
     for pokemon in data:
         p = pokemon[1]['Raw count']/float(info['number of battles'])
@@ -49,7 +58,7 @@ def diversity(q, epsilon=0):
         wgm += p**q
     return wgm**(1/(1-q))
 
-def shannon(epsilon=0):
+def shannon():
     H = 0
     for pokemon in data:
         p = pokemon[1]['Raw count']/float(info['number of battles'])
@@ -58,18 +67,32 @@ def shannon(epsilon=0):
         H -= p*math.log(p)
     return H
 
-def gini_simpson(epsilon=0):
-    return 1-simpson(epsilon)
+def gini_simpson():
+    return 1-simpson()
 
-def simpson(epsilon=0):
-    return 1/diversity(2, epsilon)
+def simpson():
+    global q
+    q = 2
+    return 1/diversity()
 
-def renyi(q, epsilon=0):
-    return math.log(diversity(q, epsilon))
+def renyi():
+    global q
+    q = 2
+    return math.log(diversity())
 
 def berger_parker():
     return data[0]['Raw count']/float(info('number of battles'))
 
+all_metrics = {'gini': gini, 'richness': richness, 'diversity': diversity, 'shannon': shannon, \
+               'gini_simpson': gini_simpson, 'simpson': simpson, 'renyi': renyi, 'berger_parker': berger_parker}
+
 if __name__ == '__main__':
-    read_file()
-    print gini()
+    month = None
+    if args.month:
+        month = args.month
+        read_file(month, args.format)
+        print all_metrics[args.metric]()
+    else: 
+        for month in all_months:
+            read_file(month, args.format)
+            print all_metrics[args.metric]()
